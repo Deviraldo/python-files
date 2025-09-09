fbaf59f90ea6d72ddd42b4bd27562b66f754052f
Deviraldo <aritradutta6426@gmail.com>
2025-09-09 20:22:46 +0530

    first commit

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,2 @@
+print("hello python")
+print("Aritra will be the richest guy ever in the world")
---
+++ main.py
@@ -0,0 +1,3 @@
+print("hello python")
+print("Aritra will be the richest guy ever in the world")
+print("Devdyuti will regret for me in her whole life")
